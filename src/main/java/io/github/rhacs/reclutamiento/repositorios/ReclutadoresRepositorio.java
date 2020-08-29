faf998eaa81ae6b5de309ba166b4d64fa7f1cc7d
Ricardo A. Hermosilla Carrillo <33528976+rhacs@users.noreply.github.com>
2020-08-28 21:50:41 -0400

    added method findTopByOrderByIdDesc

--- src/main/java/io/github/rhacs/reclutamiento/repositorios/ReclutadoresRepositorio.java
+++ src/main/java/io/github/rhacs/reclutamiento/repositorios/ReclutadoresRepositorio.java
@@ -1,5 +1,7 @@
 package io.github.rhacs.reclutamiento.repositorios;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,10 @@ import io.github.rhacs.reclutamiento.modelos.Reclutador;
 
 @Repository
 public interface ReclutadoresRepositorio extends JpaRepository<Reclutador, Long> {
+
+    /**
+     * @return el último registro ingresado al repositorio, si existe
+     */
+    public Optional<Reclutador> findTopByOrderByIdDesc();
 
 }
